a7b8a14e802c4c604cf26b1fd33ca1b92d7dfdde
hillium <yu745514916@live.com>
2025-03-02 15:44:32 +0800

    updated deps and styles

--- scripts/gen_static_pages.js
+++ scripts/gen_static_pages.js
@@ -1,53 +1,47 @@
-const pug = require('pug')
-const marked = require('marked')
-const fs = require('fs')
-const fsp = require('fs/promises')
-const path = require('path')
-const uglifycss = require('uglifycss');
-const childp = require('child_process');
+import { compile } from 'pug'
+import { use, marked } from 'marked'
+import { writeFile, open, readdir, mkdir, stat as _stat, copyFile, access, readFile } from 'fs/promises'
+import { resolve } from 'path'
+import { processString } from 'uglifycss'
+import { exec as _exec } from 'child_process'
+import hljs from 'highlight.js'
 
-marked.use({
-    highlight: function (code, language) {
-        const hljs = require('highlight.js');
-        const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
-        return hljs.highlight(validLanguage, code).value;
-    },
+use({
+   
     renderer: {
-        image(href, title, text) {
+        image({href, title, text}) {
             return `<div class="lazyimg-container container" data-src="${href}" data-alt="${text}" ${(title && `data-title="${title}"`) || ""}>
                 <div class="lazyimg-loading-placeholder container justify-center align-center">
                     <div>“${text.replace('“','‘').replace('”','’') || "加载中的图片"}”</div>
                 </div>
             </div>`
+        },
+        code({ text, lang }) {
+            const validLanguage = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return `<pre><code data-lang="${lang}">${hljs.highlight(validLanguage, text).value}</code></pre>`;
         }
     }
 })
 
 const PATH_RELEASE = './docs'
 const PATH_META_DATABASE = './articles/meta.json'
-const PATH_ARTICLE_RELEASE = path.resolve(PATH_RELEASE, "articles")
+const PATH_ARTICLE_RELEASE = resolve(PATH_RELEASE, "articles")
 const PATH_TEMPLATE = './template'
 const PATH_SOURCES = './web-resources'
-const PATH_BLOB = path.resolve(PATH_SOURCES, 'blob')
-const PATH_FAVICON = path.resolve(PATH_SOURCES, 'favicons')
-const PATH_RELEASE_SOURCES = path.resolve(PATH_RELEASE, 'web-resources')
-const PATH_RELEASE_BLOB = path.resolve(PATH_RELEASE_SOURCES, 'blob')
-const PATH_RELEASE_FAVICON = path.resolve(PATH_RELEASE_SOURCES, 'favicons')
-const compilePug = (file) => pug.compile(
-    fs.readFileSync(path.resolve(PATH_TEMPLATE, file)),
+const PATH_BLOB = resolve(PATH_SOURCES, 'blob')
+const PATH_FAVICON = resolve(PATH_SOURCES, 'favicons')
+const PATH_RELEASE_SOURCES = resolve(PATH_RELEASE, 'web-resources')
+const PATH_RELEASE_BLOB = resolve(PATH_RELEASE_SOURCES, 'blob')
+const PATH_RELEASE_FAVICON = resolve(PATH_RELEASE_SOURCES, 'favicons')
+const compilePug = async (file) => compile(
+    await readFile(resolve(PATH_TEMPLATE, file)),
     {
         basedir: PATH_TEMPLATE,
-        filename: path.resolve(PATH_TEMPLATE, file),
+        filename: resolve(PATH_TEMPLATE, file),
     })
-const TEMPLATE_ARTICLE = compilePug('article.pug')
-const TEMPLATE_INDEX = compilePug('index.pug')
-const TEMPLATE_404 = compilePug('404.pug')
-const renderTime = t => {
-    if (t instanceof Date) {
-        return t.toLocaleString()
-    }
-    return `${t}`
-}
+const TEMPLATE_ARTICLE = await compilePug('article.pug')
+const TEMPLATE_INDEX = await compilePug('index.pug')
+const TEMPLATE_404 = await compilePug('404.pug')
 
 const genIndexPage = ({ articles, pageNum, maxPageNum }) => {
     return TEMPLATE_INDEX({
@@ -58,13 +52,13 @@ const genIndexPage = ({ articles, pageNum, maxPageNum }) => {
 }
 
 const write404 = async () => {
-    await fsp.writeFile(path.resolve(PATH_RELEASE, '404.html'), TEMPLATE_404({}))
+    await writeFile(resolve(PATH_RELEASE, '404.html'), TEMPLATE_404({}))
 }
 
 const writeIndices = async (articles) => {
-    const file = await fsp.open(path.resolve(PATH_RELEASE, "index.html"), 'w')
+    const file = await open(resolve(PATH_RELEASE, "index.html"), 'w')
     await file.writeFile(genIndexPage({
-        articles: articles.map(article => ({ ...article, lastUpdated: renderTime(article.lastUpdated), createdAt: renderTime(article.createdAt) })),
+        articles: articles.map(article => ({ ...article })),
         pageNum: 1,
         maxPageNum: 1
     }))
@@ -75,14 +69,14 @@ const genArticlePage = ({ name, lastUpdated, createdAt, markdownContent, tags })
     return TEMPLATE_ARTICLE({
         name,
         tags,
-        lastUpdated: renderTime(lastUpdated),
-        createdAt: renderTime(createdAt),
+        lastUpdated: lastUpdated,
+        createdAt: createdAt,
         renderedContent: marked(markdownContent)
     })
 }
 
 const getMetaDatabase = async () => {
-    const fd = await fsp.open(PATH_META_DATABASE)
+    const fd = await open(PATH_META_DATABASE)
     const tagMapping = JSON.parse(await fd.readFile())
     await fd.close()
     return tagMapping
@@ -90,7 +84,7 @@ const getMetaDatabase = async () => {
 
 
 const writeArticlePage = async (article) => {
-    const targetFD = await fsp.open(path.resolve(PATH_ARTICLE_RELEASE, article.name + '.html'), 'w')
+    const targetFD = await open(resolve(PATH_ARTICLE_RELEASE, article.name + '.html'), 'w')
     await targetFD.writeFile(genArticlePage(article))
     await targetFD.close()
 }
@@ -98,10 +92,10 @@ const writeArticlePage = async (article) => {
 const comparing = (f) => (a, b) => f(a) < f(b)
 
 const genArticleDB = async (articlesPath) => {
-    const files = await fsp.readdir(articlesPath)
+    const files = await readdir(articlesPath)
     const metaDB = await getMetaDatabase();
     const getArticleInfo = async (file, meta) => {
-        const fd = await fsp.open(path.resolve(articlesPath, file))
+        const fd = await open(resolve(articlesPath, file))
         const stat = await fd.stat()
         const lastUpdated = stat.mtime
         const createdAt = stat.birthtime
@@ -124,27 +118,27 @@ const genArticleDB = async (articlesPath) => {
 
 const copyFiles = async (source, target) => {
     if (!await exists(target)) {
-        await fsp.mkdir(target, {recursive: true})
+        await mkdir(target, {recursive: true})
     }
-    return fsp.readdir(source)
+    return readdir(source)
         .then(arr => Promise.all(
             arr.map(async f => {
-                const stat = await fsp.stat(path.resolve(source, f));
+                const stat = await _stat(resolve(source, f));
                 if (stat.isDirectory()) {
-                    if (!await exists(path.resolve(target, f))) {
-                        await fsp.mkdir(path.resolve(target, f), {'recursive': true})
+                    if (!await exists(resolve(target, f))) {
+                        await mkdir(resolve(target, f), {'recursive': true})
                     }
-                    await copyFiles(path.resolve(source, f), path.resolve(target, f))
+                    await copyFiles(resolve(source, f), resolve(target, f))
                     return
                 }
-                await fsp.copyFile(path.resolve(source, f), path.resolve(target, f))
+                await copyFile(resolve(source, f), resolve(target, f))
             })))
 }
 
 const genWithArticles = async (articlesPath) => {
-    await fsp.mkdir(PATH_RELEASE, { recursive: true })
+    await mkdir(PATH_RELEASE, { recursive: true })
     const articleDB = await genArticleDB(articlesPath)
-    await fsp.mkdir(PATH_ARTICLE_RELEASE, { recursive: true })
+    await mkdir(PATH_ARTICLE_RELEASE, { recursive: true })
     const writingPages = Promise.all(
         articleDB
             .map(async article => {
@@ -155,7 +149,7 @@ const genWithArticles = async (articlesPath) => {
 }
 
 /** @type {(cmd: string)=>Promise<[string,string]>} */
-const exec = (cmd) => new Promise((ok, err) => childp.exec(cmd, (e, stdout, stderr) => {
+const exec = (cmd) => new Promise((ok, err) => _exec(cmd, (e, stdout, stderr) => {
     if (e != undefined && e != null) {
         err(e)
         return
@@ -166,7 +160,7 @@ const exec = (cmd) => new Promise((ok, err) => childp.exec(cmd, (e, stdout, stde
 /** @type {(path: string)=>Promise<boolean>} */
 const exists = async (path) => {
     try {
-        await fsp.access(path)
+        await access(path)
         return true
     } catch {
         return false
@@ -174,14 +168,14 @@ const exists = async (path) => {
 }
 
 const genSources = async () => {
-    await fsp.mkdir(PATH_RELEASE_SOURCES, { recursive: true })
-    const files = await fsp.readdir(PATH_SOURCES)
+    await mkdir(PATH_RELEASE_SOURCES, { recursive: true })
+    const files = await readdir(PATH_SOURCES)
     const uglifyCSSFiles = Promise.all(
         files.filter(f => /\.css$/.test(f))
             .map(async (file) => {
-                const css = (await fsp.readFile(path.resolve(PATH_SOURCES, file))).toString()
-                const uglified = uglifycss.processString(css, { uglyComments: true });
-                await fsp.writeFile(path.resolve(PATH_RELEASE_SOURCES, file), uglified)
+                const css = (await readFile(resolve(PATH_SOURCES, file))).toString()
+                const uglified = processString(css, { uglyComments: true });
+                await writeFile(resolve(PATH_RELEASE_SOURCES, file), uglified)
     }))
     const genTSFiles = exec("npx webpack build").then(([stdout, stderr]) => {
         if (stdout) { console.log(">>> webpack stdout\n", stdout, "<<< end webpack stdout") }
